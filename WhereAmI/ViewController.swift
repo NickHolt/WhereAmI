0e7fa98c62a15738864d2f7dce4df4b8bf38f610
nickholt <nickholt93@gmail.com>
2015-07-06 14:01:50 -0700

    Initial commit

---
+++ WhereAmI/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  WhereAmI
+//
+//  Created by Nick Holt on 7/6/15.
+//  Copyright (c) 2015 Nick Holt. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var hAccuracyLabel: UILabel!
+    @IBOutlet weak var vAccuracyLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = kCLDistanceFilterNone
+        
+        // Request in-use location services permission
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            manager.requestWhenInUseAuthorization()
+        }
+    
+        manager.startUpdatingLocation()
+        if CLLocationManager.headingAvailable() {
+            manager.startUpdatingHeading()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let newestLocation = locations[0] as! CLLocation
+        
+        latitudeLabel.text = NSString(format:"%.5f°", newestLocation.coordinate.latitude) as String
+        longitudeLabel.text = NSString(format:"%.5f°", newestLocation.coordinate.longitude) as String
+        altitudeLabel.text = NSString(format:"%.2fm", newestLocation.altitude) as String
+        speedLabel.text = NSString(format:"%.2fm/s", newestLocation.speed) as String
+        hAccuracyLabel.text = NSString(format:"%.2fm", newestLocation.horizontalAccuracy) as String
+        vAccuracyLabel.text = NSString(format:"%.2fm", newestLocation.verticalAccuracy) as String
+    }
+
+    func locationManager(manager: CLLocationManager!, didUpdateHeading newHeading: CLHeading!) {
+        headingLabel.text = newHeading.description
+    }
+}
+
