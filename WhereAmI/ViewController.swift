e31d328a3c226654046b8c231d51895287a3d65f
nickholt <nickholt93@gmail.com>
2015-07-07 12:05:05 -0700

    Implemented reverse geocoding.
    The app now displays basic information about the user's geocoded location, which is updated in ~60 second intervals.

--- WhereAmI/ViewController.swift
+++ WhereAmI/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    let manager = CLLocationManager()
+    let manager: CLLocationManager
+    let geocodePeriod: NSTimeInterval
+    var previousUpdateTimestamp: NSDate
     
+    @IBOutlet weak var geocodeLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var altitudeLabel: UILabel!
@@ -20,6 +23,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var hAccuracyLabel: UILabel!
     @IBOutlet weak var vAccuracyLabel: UILabel!
     
+    required init(coder aDecoder: NSCoder)  {
+        manager = CLLocationManager()
+        geocodePeriod = 60 // seconds
+        previousUpdateTimestamp = NSDate(timeIntervalSinceReferenceDate: -geocodePeriod)
+        
+        super.init(coder:aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,12 +57,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         let newestLocation = locations[0] as! CLLocation
         
+        // Update view
         latitudeLabel.text = NSString(format:"%.5f°", newestLocation.coordinate.latitude) as String
         longitudeLabel.text = NSString(format:"%.5f°", newestLocation.coordinate.longitude) as String
         altitudeLabel.text = NSString(format:"%.2fm", newestLocation.altitude) as String
         speedLabel.text = NSString(format:"%.2fm/s", newestLocation.speed) as String
         hAccuracyLabel.text = NSString(format:"%.2fm", newestLocation.horizontalAccuracy) as String
         vAccuracyLabel.text = NSString(format:"%.2fm", newestLocation.verticalAccuracy) as String
+        
+        // Perform reverse geocode (if needed)
+        let newTimestamp = newestLocation.timestamp
+        if newTimestamp.timeIntervalSinceDate(previousUpdateTimestamp) >= geocodePeriod {
+            CLGeocoder().reverseGeocodeLocation(newestLocation) {
+                let placemark = $0.0[0] as! CLPlacemark
+                let error = $0.1
+                
+                if error != nil {
+                    println("Reverse geocoding failed with error: " + error.localizedDescription)
+                } else {
+                    self.geocodeLabel.text = placemark.name + ", " + placemark.locality
+                }
+            }
+        }
+        
+        previousUpdateTimestamp = newTimestamp
     }
 
     func locationManager(manager: CLLocationManager!, didUpdateHeading newHeading: CLHeading!) {
